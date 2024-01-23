c5ac91f6a88e39a4527aaa2234faff25f2790903
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-01-23 08:44:01 +0700

    vidio 3
    java script tipe data

---
+++ javaScript/js/tipedata.js
@@ -0,0 +1,19 @@
+let angka = 10 ;
+
+let kata = "saya belajar java script";
+
+let pecahan = 2.5;
+
+console.log (angka);
+
+console.log (typeof angka);
+
+console.log (1 / 0);
+
+console.log("javascript" / 2 + 5);
+
+let isi = "belajar";
+
+console.log (isi + kata);
+
+console.log (`smk buduran ${angka} ${kata} ${isi}`);
